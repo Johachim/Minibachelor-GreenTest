9a6f71d01b4bf78cf5780086211f86513d54d396
Sara <cph-sf131@cphbusiness.dk>
2021-05-06 17:18:43 +0200

    JSON data med mad + test

--- scripts/beregner.js
+++ scripts/beregner.js
@@ -1,1 +1,19 @@
-console.log("Tester");
+function readJSONtest() {
+    fetch('../data/mad_data.json')
+    .then(response => {
+        if(!response.ok){
+            throw new ("HTTP error " + response.status + " text: " + response.statusText)
+        }
+        return response.json();
+    }).then(data => {
+        // data = JSON.parse(json);
+        for(let i in data){
+            console.log(data[i])
+        }
+        // console.log(json[0])
+    }).catch(error =>{
+        console.log("Error:" + error.status)
+    })
+}
+
+readJSONtest();
