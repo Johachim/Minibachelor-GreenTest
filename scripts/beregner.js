73faafce3cf66f36b4224f1c504f06ba58c92852
Sara <cph-sf131@cphbusiness.dk>
2021-05-11 18:40:55 +0200

    Madberegner fortsat

--- scripts/beregner.js
+++ scripts/beregner.js
@@ -1,5 +1,3 @@
-// Link for data: https://agro.au.dk/fileadmin/user_upload/Mogensen_et_al_2016_Foedevarernes_klimaaftryk.pdf
-console.log("Test");
 import Beregner from "./classes/beregner_class.js";
 
 //Set up
@@ -8,7 +6,9 @@ const select2 = document.querySelector("#secondSelected");
 const submitbtn = document.querySelector('#beregnSubmit')
 const result = document.querySelector('#resultat')
 const beregner = new Beregner(select1, select2, '../data/mad_data.json', submitbtn, result);
-beregner.importValuesToSelects();
+beregner.setup();
+
+beregner.test();
 
 //Add EventListener for "Beregn" button
 submitbtn.addEventListener('click', function(){
--- scripts/classes/beregner_class.js
+++ scripts/classes/beregner_class.js
@@ -5,35 +5,109 @@ export default class Beregner{
         this.url = valuesURL;
         this.result = result;
         this.submitBtn = submitBtn;
+        this.madliste = this.fetchValues();
+        this.counter = 0;
     }
 
-    importValuesToSelects(){
-        this.fetchValues(this.firstSelector);
-        this.fetchValues(this.secondSelector);
+    setup(){
+        this.importValuesToSelects(this.firstSelector);
+        this.importValuesToSelects(this.secondSelector);
     }
 
-    fetchValues = function (select) {
-        fetch(this.url)
-        .then(response => {
-            if(!response.ok){
-                throw new ("HTTP error " + response.status + " text: " + response.statusText)
-            }
-            return response.json();
-        }).then(data => {
-            // data = JSON.parse(json);
-            for(let i in data){
+    importValuesToSelects(select){
+        this.madliste.then(function(result){
+            result.forEach(mad => {
                 let option = document.createElement("option");
-                option.text = data[i].navn;
-                option.value = data[i].navn;
+                option.text = mad.navn;
+                option.value = mad.id;
                 select.append(option);
-            }
-            // console.log(json[0])
-        }).catch(error =>{
-            console.log("Error:" + error.status)
-        })
+            });
+        });
     }
+
+    fetchValues = async () => {
+        let array = [{id: 0, navn: "Vælg en madvare", klimaftryk: null, imported: null, kategori: null}];
+        const response = await fetch(this.url);
+        const data = await response.json();
+        data.forEach(mad =>{
+            array.push(mad);
+        });
+        
+        // fetch(this.url)
+        // .then(response => {
+        //     if(!response.ok){
+        //         throw new ("HTTP error " + response.status + " text: " + response.statusText)
+        //     }
+        //     return response.json();
+        // }).then(data => {
+        //     for(let i in data){
+        //         let madvare = {
+        //             navn: data[i].navn,
+        //             klimaftryk: data[i].klimaftryk,
+        //             imported: data[i].import,
+        //             kategori: data[i].kategori
+        //         }
+        //         array.push(madvare);
+        //     }
+        // }).catch(error =>{
+        //     console.log("Error:" + error.status)
+        // });
+        return array;
+    };
 
     beregn = function(){
+        let result = this.result;
+        if(this.firstSelector.value == 0 || this.secondSelector.value == 0){
+            let resultTemplate = `
+                <div class="col-12"><h2 class="align_center colour_dark_grey">Vælg 2 madvarer for at se resultatet</h2></div>
+            `
+            result.innerHTML = resultTemplate;
+        } else if(this.firstSelector.value == this.secondSelector.value){
+            let resultTemplate = `
+                <div class="col-12"><h2 class="align_center colour_dark_grey">Vælg 2 forskellige madvarer for at se resultatet</h2></div>
+            `
+            result.innerHTML = resultTemplate;
+        }else{
+            let first = this.firstSelector.value;
+            let second = this.secondSelector.value;
+            //Collect objects in madliste
+            this.madliste.then(function(madvarer){
+                let firstKlima = madvarer[first].klimaftryk + madvarer[first].import;
+                let secondKlima = madvarer[second].klimaftryk + madvarer[second].import;
+                let first_colour = "";
+                let second_colour = "";
+
+                //Define colours for text
+                if(firstKlima < secondKlima){
+                    first_colour = "primary_green";
+                    second_colour = "dark_grey";
+                } else if (firstKlima > secondKlima){
+                    first_colour = "dark_grey";
+                    second_colour = "primary_green";
+                }else {
+                    first_colour = "dark_grey";
+                    second_colour = "dark_grey";
+                }
+
+                let resultTemplate = `
+                    <div class="row">
+                        <h3 class="colour_${first_colour} col-3">${madvarer[first].navn}</h3>
+                        <h3 class="colour_dark_grey align_center col-2">vs</h3>
+                        <h3 class="colour_${second_colour} col-3">${madvarer[second].navn}</h3>
+                    </div>
+                    <div class="col-8">
+
+                    </div>
+                    <div class="col-4 align_center">
+                        <p>${madvarer[first].navn}s samlet<br/>klimaaftryk* er</p>
+                        <h3 class="colour_${first_colour}"> %</h3>
+                        <p>end ${madvarer[second].navn}</p>
+                        <small>*Gælder kun importede fødevarer. Der lægges omkring 0.2 noget oven i pr fødevarer i transport</small>
+                    </div>
+                `;
+                result.innerHTML = resultTemplate;
+            });
+        }
         this.result.classList.remove("hidden");
     }
 }
