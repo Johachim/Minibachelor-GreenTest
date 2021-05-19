8e57c5bdb1267e28f4a8cb1e92149057f693fcc4
Johachim <joachimgul@yahoo.dk>
2021-05-19 13:13:59 +0200

    pagination og billeder på quiz, mangler finpudsning af resultater

--- scripts/Quiz.js
+++ scripts/Quiz.js
@@ -6,15 +6,20 @@
             
             for(letter in currentQuestion.answers){
                 answers.push(`
-                <label>
+                <label class="labelcontainer">
                 <input type="radio" name="question${questionNumber}" value="${letter}">
                 ${letter} :
                 ${currentQuestion.answers[letter]}
                 </label>`)
             }
             output.push(`
-            <div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div>`);
+            <div class="slide">
+            <p class="question-amount"> Spørgsmål ${currentQuestion.qNumber} af 8</p> 
+            <img src="${currentQuestion.imgFile}" alt="${currentQuestion.imgFile}" width="120" height="120"></img>
+            <h4 class="question"> ${currentQuestion.question} </h4>
+            <div class="answers"> ${answers.join('')} </div>
+            </div>`);
+            
         }
         
         );
@@ -42,10 +47,10 @@
             else if(userAnswer === currentQuestion.correctAnswer2) {
                 numCorrect+=6;
 
-                answerContainers[questionNumber].style.color = "yellow"
+                answerContainers[questionNumber].style.color = "darkgoldenrod"
             }
             else{
-                answerContainers[questionNumber].style.color = "red";
+                answerContainers[questionNumber].style.color = "firebrick";
             }
         });
         resultsContainer.innerHTML = `<br/>
@@ -54,21 +59,26 @@
         <p>Læs <a href="./Quiz_reasoning.html">her</a> for at se hvordan du kan gøre din hverdag grønnere!</p>
         `
     }
-    
+    //DOM and questions
     const quizContainer = document.getElementById('quizdiv');
     const resultsContainer = document.getElementById('results');
     const submitButton = document.getElementById('submit');
     const quizQuestions = [
         {
+            qNumber: "1",
+            imgFile: "./images/El_Bil.png",
             question: "Bruger du primært en El-bil eller en benzin/diesel bil (den bil der bruges oftest om du bliver kørt eller kører)?",
             answers: {
                 a: "Benzin",
                 b: "Diesel",
                 c: "El"
         },
-        correctAnswer: "c"
+        correctAnswer: "c",
+        correctAnswer2: "b"
     },
     {
+        qNumber: "2",
+        imgFile: "./images/Grøntsag.png",
         question: "Har du nogle særlige spisevaner?",
         answers: {
             a: "Veganer",
@@ -76,9 +86,12 @@
             c: "Spiser ikke rødt kød",
             d: "Spiser alt"
         },
-        correctAnswer: "a"
+        correctAnswer: "a",
+        correctAnswer2: "b"
     },
     {
+        qNumber: "3",
+        imgFile: "./images/Normal_Bruser.png",
         question: "Hvor lang tid bruger du normalt på et bad?",
         answers: {
             a: "< 5 minutter",
@@ -89,6 +102,8 @@
         correctAnswer: "a"
     },
     {
+        qNumber: "4",
+        imgFile: "./images/blad.png",
         question: "Hvor mange indendørs planter har du i din bolig?",
         answers: {
             a: "0",
@@ -100,6 +115,8 @@
         correctAnswer2: "c"
     },
     {
+        qNumber: "5",
+        imgFile: "./images/Plastik_Pose.png",
         question: "Genbrug af affald. Vælg det svar der passer bedst for hvad du gør.",
         answers: {
             a: "Genbruger: Plastik, glas, tøj, metal, pap, papir, madaffald (kompost)",
@@ -111,6 +128,8 @@
         correctAnswer2: "b"
     },
     {
+        qNumber: "6",
+        imgFile: "./images/Slukket_Pære.png",
         question: "Slukker du lyset når du forlader et rum?",
         answers: {
             a: "Ja",
@@ -119,6 +138,8 @@
         correctAnswer: "a"
     },
     {
+        qNumber: "7",
+        imgFile: "./images/Gas_Gril.png",
         question: "Hvis du griller hvordan foregår det så oftest?",
         answers: {
             a: "Kulgril",
@@ -129,6 +150,8 @@
         correctAnswer2: "b"
     },
     {
+        qNumber: "8",
+        imgFile: "./images/Stof_Pose.png",
         question: "Bruger du genbrugelige genstande til daglig?",
         answers: {
             a: "Taske/Bærepose med til indkøb",
@@ -144,5 +167,41 @@
 
 // display quiz 
 buildQuiz();
-
 submitButton.addEventListener('click', showResults);
+
+
+//Pagination
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currSlide = 0;
+
+showSlide = (n) => {
+    slides[currSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currSlide = n;
+    if(currSlide === 0){
+        previousButton.style.display = "none";
+    }
+    else{
+        previousButton.style.display = "inline-block";
+    }
+    if(currSlide === slides.length-1){
+        nextButton.style.display = "none";
+    }
+    else{
+        nextButton.style.display = "inline-block";
+    }
+}
+
+showSlide(currSlide);
+
+showNextSlide = () => {
+    showSlide(currSlide + 1)
+}
+showPreviousSlide = () => {
+    showSlide(currSlide - 1)
+}
+
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
